a1798dea754d2bc3b20ff7a0c86978e8aeb93aae
vanessa.prado <vanessapradoaws@gmail.com>
2025-08-25 21:46:41 -0400

    terraform - criação script ec2

--- main.tf
+++ main.tf
@@ -14,3 +14,41 @@ provider "aws" {
     region = "us-east-1"
     profile = "bia"  
 }
+
+resource "aws_security_group" "bia-dev" {
+  name        = "bia-dev-tf"
+  description = "Regra para a instancia de trabalho bia-dev-tf"
+  vpc_id      = "vpc-0d6774d8052299f6c"
+
+  ingress {
+    description      = "Liberado 3001 para o mundo"
+    from_port        = 3001
+    to_port          = 3001 
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_instance" "bia-terraform" {
+  ami           = "ami-02f3f602d23f1659d"
+  instance_type = "t3.micro"
+  tags = {
+    ambiente = "producao"
+    Name     = "bia-terraform"
+  }
+  vpc_security_group_ids = [aws_security_group.bia-dev.id]
+  root_block_device {
+    volume_size = 10
+  }
+}
